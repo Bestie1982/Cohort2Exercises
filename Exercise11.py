eb7f503ea86f0b5021d3841506fca6a5dedc2bd4
Bestie1982 <124299983+Bestie1982@users.noreply.github.com>
2023-02-27 17:32:06 +0000

    Add files via upload

---
+++ Exercise11.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import math
+import re
+#Exercise 1
+Belgium = ('Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German')
+#print(len(Belgium))
+print('-' * (len(Belgium)))
+#Exercise 2
+belgium_underscore = Belgium.replace(",", "_")
+print(belgium_underscore)
+
+
+
+
+
+#str.replace(Belgium,)
+#print(Belgium)
